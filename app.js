0d561faa22c20a42543da5b5ff05d3147174bc24
ajLapid718 <ajLapid718@gmail.com>
2020-01-15 17:36:50 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,5 @@
+require("./secrets");
+
+console.log(process.env.HELLO);
+console.log(process.env.API_KEY);
+console.log(process.env.PASSWORD);
---
+++ secrets.js
@@ -0,0 +1,3 @@
+process.env.HELLO = "Hello!";
+process.env.API_KEY = "QWERTYUIOP";
+process.env.PASSWORD = "this is a password";
